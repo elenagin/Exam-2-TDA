ebb96f92dc25f93409ebf296d4c77693b23e2922
lennygin <lennginzan@gmail.com>
2020-04-23 16:06:54 -0500

    6 bits encoding

--- controller.c
+++ controller.c
@@ -8,32 +8,34 @@
 int main()
 {
     int data[10];
-    int dataatrec[10], c, c1, c2, c3, i;
+    int receivedData[10], c, c1, c2, c3, i;
 
-    printf("Enter 4 bits of data one by one\n");
-    scanf("%d", &data[0]);
-    scanf("%d", &data[1]);
-    scanf("%d", &data[2]);
-    scanf("%d", &data[4]);
+    printf("Enter 6 bits of data one by one\n");
+    scanf("%d", &data[9]);
     scanf("%d", &data[8]);
-    
+    scanf("%d", &data[6]);
+    scanf("%d", &data[5]);
+    scanf("%d", &data[4]);
+    scanf("%d", &data[2]);
 
     //Calculation of even parity
-    data[6] = data[0] ^ data[2] ^ data[4];
-    data[5] = data[0] ^ data[1] ^ data[4];
-    data[3] = data[0] ^ data[1] ^ data[2];
+    data[0] = data[2] ^ data[4] ^ data[6] ^ data[8]; //p1
+    data[1] = data[2] ^ data[5] ^ data[6] ^ data[9]; //p2
+    data[3] = data[4] ^ data[5] ^ data[6]; //p4
+    data[7] = data[8] ^ data[9]; //p8
 
     printf("\nEncoded data is\n");
-    for (i = 0; i < 7; i++)
+    for (i = 9; i >= 0; i--)
         printf("%d", data[i]);
 
     printf("\n\nEnter received data bits one by one\n");
-    for (i = 0; i < 7; i++)
-        scanf("%d", &dataatrec[i]);
+    for (i = 9; i >= 0; i--)
+        scanf("%d", &receivedData[i]);
 
-    c1 = dataatrec[6] ^ dataatrec[4] ^ dataatrec[2] ^ dataatrec[0];
-    c2 = dataatrec[5] ^ dataatrec[4] ^ dataatrec[1] ^ dataatrec[0];
-    c3 = dataatrec[3] ^ dataatrec[2] ^ dataatrec[1] ^ dataatrec[0];
+    c0 = receivedData[2] ^ receivedData[4] ^ receivedData[6] ^ receivedData[8];
+    c1 = receivedData[2] ^ receivedData[5] ^ receivedData[6] ^ receivedData[9];
+    c3 = receivedData[4] ^ receivedData[5] ^ receivedData[6];
+    c7 = receivedData[7] = receivedData[8] ^ receivedData[9]; 
     c = c3 * 4 + c2 * 2 + c1;
 
     if (c == 0)
@@ -50,19 +52,19 @@ int main()
 
         printf("\nData received : ");
         for (i = 0; i < 7; i++)
-            printf("%d", dataatrec[i]);
+            printf("%d", receivedData[i]);
 
         printf("\nCorrect message is\n");
 
         //if errorneous bit is 0 we complement it else vice versa
-        if (dataatrec[7 - c] == 0)
-            dataatrec[7 - c] = 1;
+        if (receivedData[7 - c] == 0)
+            receivedData[7 - c] = 1;
         else
-            dataatrec[7 - c] = 0;
+            receivedData[7 - c] = 0;
 
         for (i = 0; i < 7; i++)
         {
-            printf("%d", dataatrec[i]);
+            printf("%d", receivedData[i]);
         }
 
         printf("\n");
