ab88d68dda850d49f76bcaa676591c907ffae96a
lennygin <lennginzan@gmail.com>
2020-04-24 09:57:14 -0500

    exam I sent

--- controller.c
+++ controller.c
@@ -9,13 +9,16 @@
 #include <stdlib.h>
 #include <string.h>
 
-
-
 int main()
 {
-    int data[10];
-    int receivedData[10], c, c1, c2, c3, i;
+    int data[7];
+    int receivedData[7], c, c1, c2, c4, i;
+    int errorBit;
+    uint8_t num = 3;
 
+    //printf("Enter data\n");
+    //scanf("%s", &num);
+    //binary(num);
 
     printf("Enter 4 bits one by one (big to small)\n");
     scanf("%d", &data[6]);
@@ -27,18 +30,29 @@ int main()
     data[1] = data[2] ^ data[5] ^ data[6]; //p2
     data[3] = data[4] ^ data[5] ^ data[6]; //p4
 
-    printf("\nEncoded : ");
+    printf("\nEncoded: ");
     for (i = 6; i >= 0; i--)
         printf("%d", data[i]);
 
-    printf("\n\nEnter received bits one by one (big to small)\n");
+    printf("\n\nWhich bit will you assign the error to? [6:0].\n");
+    scanf("%d", &errorBit);
+
     for (i = 6; i >= 0; i--)
-        scanf("%d", &receivedData[i]);
+        receivedData[i] = data[i];
+
+    if (receivedData[errorBit] == 1)
+    {
+        receivedData[errorBit] = 0;
+    }
+    else
+    {
+        receivedData[errorBit] = 1;
+    }
 
     c1 = receivedData[2] ^ receivedData[4] ^ receivedData[6]; //p1
     c2 = receivedData[2] ^ receivedData[5] ^ receivedData[6]; //p2
-    c3 = receivedData[4] ^ receivedData[5] ^ receivedData[6]; //p4
-    c = c3 * 4 + c2 * 2 + c1;
+    c4 = receivedData[4] ^ receivedData[5] ^ receivedData[6]; //p4
+    c = c4 * 4 + c2 * 2 + c1;
 
     if (c == 0)
     {
@@ -58,10 +72,10 @@ int main()
 
         printf("\nCorrect is: \n");
 
-        if (receivedData[7 - c] == 0)
-            receivedData[7 - c] = 1;
+        if (receivedData[errorBit] == 0)
+            receivedData[errorBit] = 1;
         else
-            receivedData[7 - c] = 0;
+            receivedData[errorBit] = 0;
 
         for (i = 6; i >= 0; i--)
         {
--- model.h
+++ model.h
@@ -5,3 +5,10 @@
 #include <math.h>
 
 
+void binary(uint8_t n)
+{
+    if (n > 1)
+        binary(n / 2);
+
+    printf("%d", n % 2);
+}
